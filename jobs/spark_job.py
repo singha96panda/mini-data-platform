feca286a577f0286e169ceb8881402a67713ce15
singha96panda <60964399+singha96panda@users.noreply.github.com>
2026-09-16 20:14:02 +0530

    Phase 2: ETL Pipeline Built

--- jobs/spark_job.py
+++ jobs/spark_job.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ["HADOOP_HOME"] = r"C:\hadoop"
+os.environ["hadoop.home.dir"] = r"C:\hadoop"
+
+
 from pyspark.sql import SparkSession
 from framework.yaml_parser import load_config
 from framework.transformation_engine import apply_transformations
@@ -7,6 +13,10 @@ config = load_config("configs/employee_etl.yaml")
 spark = (
     SparkSession.builder
     .appName("EmployeeETL")
+    .config(
+        "spark.jars",
+        "drivers/postgresql-42.7.13.jar"
+    )
     .getOrCreate()
 )
 
@@ -21,13 +31,29 @@ df = apply_transformations(
     config["transformations"]
 )
 
-target_path = config["target"]["file"]
-
-### df.write.mode("overwrite").csv(
-###    target_path,
-###    header=True
-###)
-
 df.show()
+
+target = config["target"]
+
+jdbc_url = (
+    f"jdbc:postgresql://"
+    f"{target['host']}:{target['port']}/"
+    f"{target['database']}"
+)
+
+properties = {
+    "user": target["user"],
+    "password": target["password"],
+    "driver": "org.postgresql.Driver"
+}
+
+df.write.jdbc(
+    url=jdbc_url,
+    table=target["table"],
+    mode="overwrite",
+    properties=properties
+)
+
+print("Data loaded to PostgreSQL successfully")
 
 spark.stop()
